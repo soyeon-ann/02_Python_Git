c8170a3665952c0b3b1b8908a711fe5eab60f179
soyeon-ann <ansy39@naver.com>
2024-12-12 02:58:00 +0900

    Update 1122 practice.py

--- 1122 practice.py
+++ 1122 practice.py
@@ -1,2 +1,13 @@
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
+data =  pd.read_csv('./data/1_pima.csv')
+print(data)
+
+X = array[:,0:8]
+print(X, "\nX shpae is ", X.shape)
+print("-----------------------")
+Y = array[:,8]
+print(Y, ''\nY shpae is ", Y.shape)
+
+MinMaxScaler
